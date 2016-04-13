e092042b4c4fe780497d7ede8bb82c6cac271ce5
JOG-CLT <ortegagingrich@gmail.com>
2016-04-13 14:12:01 -0700

    Updated runserver.sh script to add optional argument allowing the django test server to be run on the local host.

--- django_project/runserver.sh
+++ django_project/runserver.sh
@@ -1,5 +1,14 @@
 #first get local ip address
-INET_ADDR=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
+if [ "$1" != "local" ]; then 
+	INET_ADDR=`/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`
+else
+	INET_ADDR="127.0.0.1"
+fi
+
 PORT='8000'
 
-python manage.py runserver $INET_ADDR:$PORT
+{
+	python manage.py runserver $INET_ADDR:$PORT
+} || {
+	echo "Cannot start server; not connected to eth0."
+}
