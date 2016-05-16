09f700f0fa34b419a94af3e45e5edf4a6cf1421f
JOG-CLT <ortegagingrich@gmail.com>
2016-05-16 10:32:22 -0700

    Added some more custom exceptions

--- django_project/inventory/exceptions.py
+++ django_project/inventory/exceptions.py
@@ -2,6 +2,8 @@
 Custom exceptions for the inventory app
 """
 
+# Item ID errors
+
 class InvalidIDError(Exception):
 	"""
 	Exceptions resulting from invalid object IDs
@@ -20,15 +22,55 @@ class InvalidItemError(InvalidIDError):
 	pass
 
 
-class InvalidDateError(Exception):
+# Invalid Value errors
+
+class InvalidValueError(Exception):
 	"""
-	Exceptions resulting from invalid dates
+	Exceptions resulting from some sort of invalid value
 	"""
-	def __init__(self, bad_date, *args, **kwargs):
+	value_descriptor = 'value'
+	requirements_message = ''
+	
+	def __init__(self, bad_value=None, *args, **kwargs):
 		Exception.__init__(self, *args, **kwargs)
-		self.bad_date = bad_date
+		self.bad_value = bad_value
+	
+	def __getattr__(self, name):
+		value_name = 'bad_{}'.format(type(self).value_descriptor)
+		if name == value_name:
+			return self.bad_value
+		
+		return Exception.__getattribute__(self, name)
+	
+	@property
+	def message(self):
+		message = '"{}" is not a valid {}.  ' + type(self).requirements_message
+		value_descriptor = type(self).value_descriptor
+		return message.format(self.bad_value, value_descriptor)
 
 
+class InvalidDateError(InvalidValueError):
+	value_descriptor = "date"
+
+class InvalidEmailError(Exception):
+	value_descriptor = "email"
+
+class InvalidPasswordError(InvalidValueError):
+	value_descriptor = 'password'
+	requirements_message = 'Valid passwords must contain at least 6 characters.'
+
+class InvalidUsernameError(InvalidValueError):
+	value_descriptor = 'username'
+	requirements_message = 'Valid usernames must contain at least 5 characters.'
+
+class UnavailableUsernameError(InvalidUsernameError):
+	@property
+	def message(self):
+		message = 'The username "{}" is not available.'
+		return message.format(self.bad_username)
+
+
+# Generic Creation Errors
 
 class ObjectCreateError(Exception):
 	"""
@@ -43,6 +85,9 @@ class ItemTypeCreateError(ObjectCreateError):
 	pass
 
 class ItemCreateError(ObjectCreateError):
+	pass
+
+class UserCreateError(ObjectCreateError):
 	pass
 
 
