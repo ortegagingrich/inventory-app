8933c983c9b7d752b4b4fafa77c7d0a0e6ba833b
JOG-CLT <ortegagingrich@gmail.com>
2016-06-05 19:14:05 -0700

    Added template tag for search forms.

---
+++ django_project/search/templatetags/search_form.py
@@ -0,0 +1,23 @@
+from django import template
+
+
+register = template.Library()
+
+@register.inclusion_tag('search/search_form.html')
+def search_form(request, search_fields, instructions=None):
+	"""
+	This is a custom template tag which generates a search submit form.  The
+	'search_fields' argument is a dictionary whose keys are strings containing
+	the names of the text input elements and whose corresponding values are
+	strings which will serve as labels applied to these input elements
+	"""
+	
+	context = {
+		'search_fields': search_fields,
+		'submit_url': request.path,
+		'instructions': instructions,
+	}
+	
+	return context
+
+
