11ba8479c7acf9f636791defbb5f2bc8944a440e
JOG-CLT <ortegagingrich@gmail.com>
2016-06-08 20:02:26 -0700

    Added some tests for loading the search test database.

--- django_project/search/tests.py
+++ django_project/search/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+
+# Tests related to the population of test/demo data.
+
+class DictionaryDataImportTest(TestCase):
+	def setUp(self):
+		refresh_database()
+		git = DictionaryEntry(word="Jacob", definition="Some jerk.")
+		git.save()
+	
+	
+	def test_contents(self):
+		matches = len(DictionaryEntry.objects.filter(word__icontains='jacob'))
+		self.assertEqual(matches, 9)
