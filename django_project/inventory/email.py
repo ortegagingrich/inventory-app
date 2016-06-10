ba8c3880d52847f3b1b06c78a1ba5f83c30c4f06
JOG-CLT <ortegagingrich@gmail.com>
2016-06-10 04:34:17 -0700

    Added email function for expiration reports.

--- django_project/inventory/email.py
+++ django_project/inventory/email.py
@@ -3,6 +3,7 @@ Contains routines for sending emails to users.
 """
 from django.core.mail import EmailMessage
 from django.core.urlresolvers import reverse
+from django.template.loader import render_to_string
 
 from django_project import settings
 
@@ -11,6 +12,49 @@ import inventory.exceptions
 
 DOMAIN = settings.DOMAIN
 FROM_EMAIL = 'ortegagingrich@gmail.com'
+
+
+def send_expiration_report(user, new_expired, old_expired=None, 
+                           new_warning=None, old_warning=None):
+	"""
+	If appropriate, sends an email to the provided user indicating recently
+	expired or soon to expire items.
+	"""
+	if new_warning == None:
+		new_warning = []
+	if old_warning == None:
+		old_warning = []
+	if old_expired == None:
+		old_expired = []
+	
+	# If no recently expired items, do nothing
+	if max(len(new_expired), len(new_warning)) == 0:
+		return
+	
+	# list of all items that will soon expire
+	warning = new_warning + old_warning
+	context = {
+		'user': user,
+		'new_expired': new_expired,
+		'old_expired': old_expired,
+		'warning': warning,
+	}
+	template = 'inventory/email/expiration_report.html'
+	
+	
+	subject = 'Inventory Expiration Report'
+	message = render_to_string(template, context)
+	
+	if not settings.DEBUG:
+		email = user.email
+	else:
+		email = FROM_EMAIL
+	
+	mail = EmailMessage(subject, message, FROM_EMAIL, [email])
+	mail.content_subtype = 'html'
+	
+	mail.send()
+
 
 def send_temporary_password(user, temporary_password):
 	"""
